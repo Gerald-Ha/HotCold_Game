dc2c827b0d1cc629ef842ecf88e9c72ab593cec7
Gerald-Ha <53166232+Gerald-Ha@users.noreply.github.com>
2024-06-17 15:40:41 +0200

    Projektdateien hinzufügen.

---
+++ GX_HotCold/WinWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace GX_HotCold
+{
+    public partial class WinWindow : Window
+    {
+        public WinWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
